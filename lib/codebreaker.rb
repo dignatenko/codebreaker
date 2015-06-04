c928f6d6c579a50dace947e2fae04882d0169ebf
dignatenko <d.i.m.o.n.vamp@gmail.com>
2015-06-04 18:45:25 +0300

    change codebreaker file

--- lib/codebreaker.rb
+++ lib/codebreaker.rb
@@ -1,4 +1,9 @@
-Dir["./codebreaker/*.rb"].each {|file| require file }
+# Dir["./codebreaker/*.rb"].each {|file| require file }
+require "codebreaker/version"
+require "codebreaker/compare"
+require "codebreaker/computer"
+require "codebreaker/menu"
+
 
 module Codebreaker
   # Menu.new.main
